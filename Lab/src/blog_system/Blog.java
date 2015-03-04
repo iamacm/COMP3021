220992b0e9ef47bff38df6a8b287482d645852f5
cmauaa <cmauaa@CSL4WK14.win2k.cs.ust.hk>
2015-03-04 13:10:04 +0800

    Lab 3 tasks completed

---
+++ Lab/src/blog_system/Blog.java
@@ -0,0 +1,88 @@
+package blog_system;
+import java.util.ArrayList;
+
+import blog_system.User;
+
+public class Blog {
+	private User user;
+	private ArrayList<Post> allPosts;
+	
+	/**
+	 * @brief Constructor
+	 * @param user
+	 */
+	public Blog(User user) {
+		this.user = user;
+		this.allPosts = new ArrayList<Post>();
+	}
+	
+	/**
+	 * @brief Set the user of the blog
+	 * @param user
+	 */
+	public void setUser(User user) {
+		
+	}
+	
+	/**
+	 * @brief Get the current user
+	 * @return
+	 */
+	public User getUser() {
+		return this.user;
+	}
+	
+	/**
+	 * @brief Create a new post in the blog
+	 * @param p
+	 */
+	public void post(Post p) {
+		allPosts.add(p);
+		System.out.println("A new post: ");
+		System.out.println(p);
+	}
+	
+	/**
+	 * @brief List all posts in the blog.
+	 */
+	public void list() {
+		System.out.print(this);
+	}
+	
+	/**
+	 * @brief Dete a post from the blog
+	 * @param index
+	 */
+	public void delete(int index) {
+		index -= 1;
+		if (index >= 0 && index < allPosts.size()) {
+			allPosts.remove(index);
+		} else {
+			System.out.println("Illegal deletion!");
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String output = "";
+		int i = 0;
+		for (final Post p: allPosts) {
+			++i;
+			output += "Post[" + i + "]:" + p + "\n";
+		}
+		return output;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this.getClass() != o.getClass()) {
+			return false;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return 0;
+	}
+}
--- Lab/src/blog_system/Main.java
+++ Lab/src/blog_system/Main.java
@@ -1,5 +1,8 @@
 package blog_system;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
 
 import blog_system.FriendsPost;
@@ -8,8 +11,27 @@ import blog_system.User;
 
 public class Main {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Main.lab3_main(args);
+
+	}
+
+	public String getInput() {
+		String line = "";
+		System.out.print("Enter the prompt: ");
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			line = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return line;
+	}
+	
+	private static void lab2_main(String[] args) {
 		User user = new User(1, "HELLO", "cmauaa@connect.ust.hk");
 		Date date = new Date();
 		String content = "This is my first post!";
@@ -17,8 +39,27 @@ public class Main {
 		FriendsPost postFromFriend = new FriendsPost(date, content, user);
 		System.out.println(postFromFriend);
 		System.out.println(postFromFriend.contains("first"));
-		System.out.println(postFromFriend.contains("HKUST"));
-		
+		System.out.println(postFromFriend.contains("HKUST"));		
 	}
-
+	
+	private static void lab3_main(String[] args) {
+		Main main = new Main();
+		User user = new User(1, "HELLO", "cmauaa@connect.ust.hk");
+		Date date = new Date();
+		Blog myblog = new Blog(user);
+		String prompt = null;
+		while (!(prompt = main.getInput()).equals("exit")) {
+			if (prompt.startsWith("list")) {
+				myblog.list();
+			} else if (prompt.startsWith("post")) {
+				String content = prompt.substring(5);
+				Post post = new Post(date, content);
+				myblog.post(post);
+			} else if (prompt.startsWith("delete")) {
+				String index_str = prompt.substring(7);
+				int index = Integer.parseInt(index_str);
+				myblog.delete(index);
+			}
+		}
+	}
 }
